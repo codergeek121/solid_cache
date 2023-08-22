80ac369d71bb43da2120d572d1ded45be3da5633
Donal McBreen <donal@basecamp.com>
2023-08-22 14:52:04 +0100

    Pass setup through to clusters

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,11 @@
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
+require "debug"
 require_relative "../test/dummy/config/environment"
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/migrate", __dir__)]
 ActiveRecord::Migrator.migrations_paths << File.expand_path("../db/migrate", __dir__)
 require "rails/test_help"
-require "debug"
 require "mocha/minitest"
 
 # Load fixtures from the engine
